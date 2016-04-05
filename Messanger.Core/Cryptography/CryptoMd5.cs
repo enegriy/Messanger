8c539d1293a2e3c008dab7e14e0eb0589e1f105e
Евгений Негрий <e.negriy@krasnodar.pro>
2016-04-05 11:27:45 +0300

    Добавлено шифрование по алгоритму md5

---
+++ Messanger.Core/Cryptography/CryptoMd5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messanger.Core
+{
+	/// <summary>
+	/// Шифрование по алоритму MD5
+	/// </summary>
+	class CryptoMd5 : ICryptography
+	{
+		/// <summary>
+		/// Зашифровать текст
+		/// </summary>
+		public string CryptoText(string text)
+		{
+			//Шифрование по алгоритму md5
+			MD5 md5 = MD5.Create();
+			var hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(text));
+			
+			//Массив байт собрать в строку
+			var sb = new StringBuilder();
+			foreach (var hashByte in hashBytes)
+			{
+				sb.Append(hashByte);
+			}
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ Messanger.Core/Cryptography/ICryptography.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messanger.Core
+{
+	/// <summary>
+	/// Интерфейс шифрования
+	/// </summary>
+	interface ICryptography
+	{
+		/// <summary>
+		/// Зашифровать текст
+		/// </summary>
+		string CryptoText(string text);
+	}
+}
