1377bdd2e6c6da8e44e8a4ee951e8b9886b1d7cb
Евгений Негрий <e.negriy@krasnodar.pro>
2016-04-06 17:09:47 +0300

    Работа над вьюхой сообщений

--- Messenger/App_Start/BundleConfig.cs
+++ Messenger/App_Start/BundleConfig.cs
@@ -30,6 +30,11 @@ namespace Messenger
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
 					  "~/Content/site.css"));
+
+			bundles.Add(new StyleBundle("~/bundles/selectizecss").Include(
+					  "~/Content/selectize.bootstrap3.css"));
+
+			
 		}
 	}
 }
